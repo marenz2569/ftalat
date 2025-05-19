8fd79e1a22ebfb5d5bba2262c4a91244c83d40fa
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-05-19 21:30:42 +0200

    import icpe-2024-schoene-et-al-alderlake-artifact Sec-4-2 benchmark scripts

---
+++ benchmark_e_cores.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir -p 10ksamples/e-core 2> /dev/null
+
+FREQUENCIES=`cat scaling_available_frequencies`
+
+for START in $FREQUENCIES
+do
+	for TARGET in $FREQUENCIES
+	do
+		 if [ $START -eq $TARGET ] 
+	         then
+        	    echo "Skip $START -> $TARGET (same frequency)"
+	            continue
+        	 fi
+
+		# measurement for long
+                export MORE_FLAGS="-DNB_WAIT_RANDOM -DNB_WAIT_US=10000 -DNB_REPORT_TIMES=10000"
+                make
+                sudo ./ftalat -c 16 $START $TARGET > 10ksamples/e-core/${START}_${TARGET}-out_random_10000ms_10000sa.txt
+
+	done
+done
