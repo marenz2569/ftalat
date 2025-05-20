7e8f7ab4ffa3bb514b2666aee85c6bc965944c4d
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-05-20 07:32:45 +0000

    update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ # TODO: remove this once the branch is merged
+ source ~/lab_management_scripts/.venv/bin/activate
+ elab ht enable
+ elab frequency performance
+
 # measurement for long
 make clean
 export MORE_FLAGS="-DNB_WAIT_RANDOM -DNB_WAIT_US=10000 -DNB_REPORT_TIMES=10000"
@@ -14,13 +19,15 @@ for START in $FREQUENCIES
 do
 	for TARGET in $FREQUENCIES
 	do
-		 if [ $START -eq $TARGET ] 
-	         then
-        	    echo "Skip $START -> $TARGET (same frequency)"
-	            continue
-        	 fi
+		if [ $START -eq $TARGET ] 
+	        then
+        		echo "Skip $START -> $TARGET (same frequency)"
+	        	continue
+        	fi
 
-		 echo "Running $START -> $TARGET"
+		echo "Running $START -> $TARGET"
+
+		elab frequency performance
 
                 sudo ./ftalat $START $TARGET > 10ksamples/${START}_${TARGET}-out_random_10000ms_10000sa.txt
 
