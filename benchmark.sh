eb0aceedf500d69a8f49b875d4c88f09ebb8d540
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-05-26 01:04:39 +0200

    fix bug in benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -46,7 +46,7 @@ if [ $scaling_available_frequencies_found -eq 0 ]
 then
 	export MORE_FLAGS="-DNB_WAIT_RANDOM -DNB_WAIT_US=10000 -DNB_REPORT_TIMES=10000 -DFREQ_SETTER_FILE=\\\"scaling_setspeed\\\""
 else
-	export MORE_FLAGS="-DNB_WAIT_RANDOM -DNB_WAIT_US=10000 -DNB_REPORT_TIMES=10000 -DFREQ_SETTER_FILE=\\\"scaling_max_speed\\\""
+	export MORE_FLAGS="-DNB_WAIT_RANDOM -DNB_WAIT_US=10000 -DNB_REPORT_TIMES=10000 -DFREQ_SETTER_FILE=\\\"scaling_max_freq\\\""
 fi
 make
 
